7ef5a09c7a349831eef98c3b61f9bed134cefdcb
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-16 01:49:09 -0300

    raw: env constants + contract verification  + config helper

--- hh-fcc/hh-fund-me/hardhat.config.ts
+++ hh-fcc/hh-fund-me/hardhat.config.ts
@@ -1,12 +1,19 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import 'hardhat-deploy'
+import 'dotenv/config'
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || 'https://eth-sepolia'
+console.log(SEPOLIA_RPC_URL)
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
+const REPORT_GAS = process.env.REPORT_GAS
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.24',
-  defaultNetwork: 'hardhat',
-  networks: {
-    hardhat: {},
+  // solidity: '0.8.24',
+  solidity: {
+    compilers: [{ version: '0.8.24' }, { version: '0.6.6' }],
   },
   namedAccounts: {
     deployer: {
@@ -16,6 +23,31 @@ const config: HardhatUserConfig = {
     user: {
       default: 1,
     },
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      // hardhat already gives the accounts
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: false,
+  },
+  gasReporter: {
+    enabled: REPORT_GAS ? true : false,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    L1: 'polygon',
   },
 }
 
---
+++ hh-fcc/hh-fund-me/helper-hardhat-config.ts
@@ -0,0 +1,23 @@
+export interface networkConfigItem {
+  name?: string
+  ethUsdPriceFeed?: string
+  blockConfirmations?: number
+}
+
+export interface networkConfigInfo {
+  [key: string]: networkConfigItem
+}
+
+export const networkConfig: networkConfigInfo = {
+  localhost: {},
+  hardhat: {},
+  sepolia: {
+    ethUsdPriceFeed: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
+    blockConfirmations: 6,
+  },
+}
+
+export const developmentChains = ['hardhat', 'localhost']
+
+export const DECIMALS = 8
+export const INITIAL_ANSWER = 200000000000
