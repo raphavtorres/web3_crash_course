e6673c2584ea73a749e54dd78557a8e64df4f2d3
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-17 21:46:46 -0300

    test: testing constructor + fundMe

--- hh-fcc/hh-fund-me/test/unit/FundMe.test.ts
+++ hh-fcc/hh-fund-me/test/unit/FundMe.test.ts
@@ -1,12 +1,18 @@
 import { deployments, ethers, getNamedAccounts } from 'hardhat'
 
 import { FundMe, MockV3Aggregator } from '../../typechain-types'
-import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
+
+import { assert, expect } from 'chai'
 
 describe('FundMe', function () {
   let fundMe: FundMe
   let mockV3Aggregator: MockV3Aggregator
-  let deployer: SignerWithAddress
+  let deployer: string
+  let signer: HardhatEthersSigner
+  const sendValue = ethers.parseEther('1') // 1 ETH
+
+  const ethProvider = ethers.getDefaultProvider()
 
   beforeEach(async function () {
     // deploy fundme contract
@@ -15,8 +21,8 @@ describe('FundMe', function () {
     // const accounts = await ethers.getSigners()
     // const deployer = accounts[0]
 
-    const deployer = (await getNamedAccounts()).deployer
-    const signer = await ethers.getSigner(deployer)
+    deployer = (await getNamedAccounts()).deployer
+    signer = await ethers.getSigner(deployer)
 
     await deployments.fixture(['all'])
     const fundMeDeployment = await deployments.get('FundMe')
@@ -35,10 +41,45 @@ describe('FundMe', function () {
     )
   })
 
-  describe('constructor', async function () {
+  describe('constructor', function () {
     it('sets the aggregator address correctly', async function () {
-      const response = await fundMe.s_priceFeed
-      // assert.equal(response, mockV3Aggregator.ad)
+      const response = await fundMe.s_priceFeed()
+      assert.equal(response, await mockV3Aggregator.getAddress())
+    })
+  })
+
+  describe('fund', function () {
+    // test if contract fails when not enough eth is sent
+    it('Fails when not enough ETH is sent', async function () {
+      // expecting that will fail
+      await expect(fundMe.fund()).to.be.revertedWith(
+        'You need to spend more ETH!'
+      )
+    })
+
+    it('Updates the amount funded data structure', async function () {
+      await fundMe.fund({ value: sendValue })
+      // getting the amount we actually sent
+      const amountFunded = await fundMe.s_addressToAmountFunded(signer)
+      assert.equal(amountFunded.toString(), sendValue.toString())
+    })
+
+    it('Adds funder do funders array', async function () {
+      await fundMe.fund({ value: sendValue })
+      const funder = await fundMe.s_funders(0)
+      assert.equal(funder, deployer)
+    })
+  })
+
+  describe('withdraw', function () {
+    beforeEach(async function () {
+      await fundMe.fund({ value: sendValue })
+    })
+
+    it('Withdraw ETH from a single founder', async function () {
+      // Arrange
+      // Act
+      // Assert
     })
   })
 })
