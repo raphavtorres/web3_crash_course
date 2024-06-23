ef702715ea5ce60f2698379001c82bfc2c24cfd1
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-22 23:41:43 -0300

    test: started unit with forked test

---
+++ foundry-cyfrin/foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/Fundme.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        // deploying contract
+        fundMe = new FundMe();
+    }
+
+    function testDemo() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
