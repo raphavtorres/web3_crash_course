36e49969bf8d58e56675eabc0c1357cb0f6cdeea
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-25 21:02:30 -0300

    refactor: test and dpeloy script refactor

--- foundry-cyfrin/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-cyfrin/foundry-fund-me/test/FundMeTest.t.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/Fundme.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
         // deploying contract
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testDemo() public view {
@@ -18,7 +20,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
